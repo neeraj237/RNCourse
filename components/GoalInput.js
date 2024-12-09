ae1ad1c800bfc72c33307b9b25856ff69ef79e0f
neeraj237 <neeraj.237@gmail.com>
2024-12-09 20:55:13 +0530

    Adding Image and change style

--- components/GoalInput.js
+++ components/GoalInput.js
@@ -48,7 +48,7 @@ const styles = StyleSheet.create({
       textInput: {
         borderWidth: 1,
         borderColor: '#e4d0ff',
-        backgroundColor: 'e4d0ff',
+        backgroundColor: '#e4d0ff',
         color: '#120438',
         borderRadius: 6,
         width:'100%',
